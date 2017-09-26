50ffbaed9bb78a8ae1be1442c13129c199a04ccc
FYL <724319952@qq.com>
2017-09-26 19:42:07 +0800

    LevelTraversal

--- src/cn/fyl/tree/TwoLinkBinTree.java
+++ src/cn/fyl/tree/TwoLinkBinTree.java
@@ -1,5 +1,10 @@
 package cn.fyl.tree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * 以二叉链表实现二叉树的基本功能
  * Created by Fang on 2017/9/22.
@@ -8,7 +13,7 @@ public class TwoLinkBinTree<E> {
     public static class TreeNode{
         Object data;
         TreeNode left;
-        TreeNode rigth;
+        TreeNode right;
 
         public TreeNode(){}
 
@@ -16,14 +21,14 @@ public class TwoLinkBinTree<E> {
             this.data = data;
         }
 
-        public TreeNode(Object data, TreeNode left, TreeNode rigth){
+        public TreeNode(Object data, TreeNode left, TreeNode right){
             this.data = data;
             this.left = left;
-            this.rigth = rigth;
+            this.right = right;
         }
 
         public String toString(){
-            return "TreeNode[data:"+data+" left:"+left+" rigth:"+rigth+"]";
+            return "TreeNode[data:"+data+" left:"+left+" right:"+right+"]";
         }
     }
 
@@ -50,7 +55,7 @@ public class TwoLinkBinTree<E> {
         if (left && parent.left != null){
             throw new RuntimeException("该节点已有左子节点，无法添加左子节点");
         }
-        if (!left && parent.rigth != null){
+        if (!left && parent.right != null){
             throw new RuntimeException("该节点已有右子节点，无法添加右子节点");
         }
         TreeNode newNode = new TreeNode(data);
@@ -58,7 +63,7 @@ public class TwoLinkBinTree<E> {
             parent.left = newNode;
         }
         else{
-            parent.rigth = newNode;
+            parent.right = newNode;
         }
         return newNode;
     }
@@ -92,10 +97,10 @@ public class TwoLinkBinTree<E> {
         if (parent == null){
             throw new RuntimeException("该节点为空，无右子节点");
         }
-        if (parent.rigth == null){
+        if (parent.right == null){
             throw new RuntimeException("该节点的右子节点为空");
         }
-        return (E)parent.rigth.data;
+        return (E)parent.right.data;
     }
 
     public int deep(){
@@ -106,14 +111,34 @@ public class TwoLinkBinTree<E> {
         if (root == null){
             return 0;
         }
-        if (root.left == null && root.rigth == null){
+        if (root.left == null && root.right == null){
             return 1;
         }
         else{
            int leftDeep = deep(root.left);
-           int rigthDeep = deep(root.rigth);
+           int rigthDeep = deep(root.right);
            int max = leftDeep > rigthDeep ? leftDeep : rigthDeep;
            return max + 1;
         }
+    }
+
+    //层次遍历
+    public List<TreeNode> levelTraversal(){
+        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+        ArrayList<TreeNode> list = new ArrayList<TreeNode>();
+        if (root != null) {
+            queue.offer(root);
+        }
+        while (!queue.isEmpty()){
+            list.add(queue.peek());
+            TreeNode node = queue.poll();
+            if (node.left != null){
+                queue.offer(node.left);
+            }
+            if (node.right != null){
+                queue.offer(node.right);
+            }
+        }
+        return list;
     }
 }
--- src/cn/fyl/tree/TwoLinkBinTreeTest.java
+++ src/cn/fyl/tree/TwoLinkBinTreeTest.java
@@ -1,5 +1,7 @@
 package cn.fyl.tree;
 
+import java.util.List;
+
 /**
  * Created by Fang on 2017/9/22.
  */
@@ -7,9 +9,13 @@ public class TwoLinkBinTreeTest {
     public static void main(String[] args){
         TwoLinkBinTree<String> binTree = new TwoLinkBinTree("root");
         TwoLinkBinTree.TreeNode root = binTree.root();
-        binTree.addNode(root,"lefe1",true);
+        binTree.addNode(root,"left1",true);
         binTree.addNode(root,"right1",false);
-        binTree.addNode(root.left,"lefe2",true);
+        binTree.addNode(root.left,"left2",true);
         System.out.println(binTree.deep());
+        List<TwoLinkBinTree.TreeNode> list = binTree.levelTraversal();
+        for (TwoLinkBinTree.TreeNode node : list){
+            System.out.print(node.data+" ");
+        }
     }
 }
